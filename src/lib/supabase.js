6aa2428f8d67fdba2c8e634dda42f6cc99b48300
chikbob <chikbob@users.noreply.github.com>
2026-07-28 13:09:19 +0300

    chore: migrate application to Mumbai Supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const directSupabaseUrl = 'https://polnkpzxtrfuqqjohepb.supabase.co'
+const directSupabaseUrl = 'https://mqxqvyckzdqnnhkozfxk.supabase.co'
 const supabaseUrl = import.meta.env.PROD ? `${window.location.origin}/supabase` : directSupabaseUrl
-const supabasePublishableKey = 'sb_publishable_420UzA8OtUjvdrk63EXz_g_InqoHure'
+const supabasePublishableKey = 'sb_publishable_jO_2ivbmep_syPjRobOazw_CXiOvYru'
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
   auth: {
